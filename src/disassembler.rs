f0f87903495ea71c6d9b7ad64754a7231047809e
Sprite Day <spriteday0@gmail.com>
2026-09-24 00:30:40 +0300

    Added feature check for HOR64_IMM operation

--- src/disassembler.rs
+++ src/disassembler.rs
@@ -210,7 +210,7 @@ pub fn disassemble_instruction<C: ContextObject>(
         ebpf::MOV64_REG  => { name = "mov64";  desc = alu_reg_str(name, insn); },
         ebpf::ARSH64_IMM => { name = "arsh64"; desc = alu_imm_str(name, insn); },
         ebpf::ARSH64_REG => { name = "arsh64"; desc = alu_reg_str(name, insn); },
-        ebpf::HOR64_IMM  => { name = "hor64"; desc = alu_imm_str(name, insn); },
+        ebpf::HOR64_IMM  if sbpf_version.disable_lddw() => { name = "hor64"; desc = alu_imm_str(name, insn); },
 
         // BPF_PQR class
         ebpf::LMUL32_IMM  if sbpf_version.enable_pqr() => { name = "lmul32"; desc = alu_imm_str(name, insn); },
